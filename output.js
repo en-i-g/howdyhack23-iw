ffbf0af3e975bb157d8cf59d039c9126842010db
en-i-g <144547426+en-i-g@users.noreply.github.com>
2023-09-10 04:13:19 -0500

    Add files via upload

---
+++ output.js
@@ -0,0 +1,85 @@
+
+var response = [
+    {
+        "r": "Reveille sleepy, try again later"
+        //"id": 0
+    },
+    {
+        "r": "Oops! Ask again"
+        //"id": 1
+    },
+    {
+        "r": "What?"
+        //"id": 2
+    },
+    {
+        "r": "I wouldn't know"
+        //"id": 3
+    },
+    {
+        "r": "Think harder and ask again"
+        //"id": 4
+    },
+    {
+        "r": "Probably not"
+        //"id": 5
+    },
+    {
+        "r": "No. no, no, no"
+        //"id": 6
+    },
+    {
+        "r": "Outlook not so good"
+        //"id": 7
+    },
+    {
+        "r": "I think not. Bad bull"
+        //"id": 8
+    },
+    {
+        "r": "Don't count on it"
+        //"id": 9
+    },
+    {
+        "r": "For sure"
+        //"id": 10
+    },
+    {
+        "r": "Yup! Gig 'em Ag!"
+        //"id": 11
+    },
+    {
+        "r": "Whoop! Outlook is good"
+        //"id": 12
+    },
+    {
+        "r": "As I see it, yes"
+        //"id": 13
+    },
+    {
+        "r": "Without a doubt"
+        //"id": 14
+    },
+    {
+        "r": "Definitely yes"
+        //"id": 15
+    },
+    {
+        "r": "Signs point to yes"
+        //"id": 16
+    },
+    {
+        "r": "Most likely"
+        //"id": 17
+    },
+    {
+        "r": "Yes. Good bull!"
+        //"id": 18
+    },
+    {
+        "r": "It is certain"
+        //"id": 19
+    }
+]
+    
+
